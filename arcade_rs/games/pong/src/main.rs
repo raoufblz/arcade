1353ff814b8432659641a85e08a7100b285b4251
raoufblz <108367862+raoufblz@users.noreply.github.com>
2026-06-04 23:10:30 +0100

    added reset_ball + rust's way (::new)

--- arcade_rs/games/pong/src/main.rs
+++ arcade_rs/games/pong/src/main.rs
@@ -22,19 +22,30 @@ fn main() {
     let mut angle = 0;
     while angle % 90 == 0 || (angle > 75 && angle < 105) || (angle > 255 && angle < 285) {angle = rl.get_random_value(1..360);}
     let radians: f32 = angle as f32 * DEG2RAD as f32;
-    let mut direction_ball = Vector2 { x: radians.cos(), y: radians.sin() };
+    let mut direction_ball = Vector2::new(radians.cos(), radians.sin());
 
     rl.set_target_fps(90);
-    let mut position_pdl_right = Vector2 { x: SCREEN_WIDTH - PADDLE_OFFSET - RECT_WIDTH, y: (SCREEN_HEIGHT - RECT_HEIGHT)/2.0 };
-    let mut position_pdl_left = Vector2 { x: PADDLE_OFFSET, y: (SCREEN_HEIGHT - RECT_HEIGHT)/2.0 };
+    let mut position_pdl_right = Vector2::new(SCREEN_WIDTH - PADDLE_OFFSET - RECT_WIDTH, (SCREEN_HEIGHT - RECT_HEIGHT)/2.0);
+    let mut position_pdl_left = Vector2::new(PADDLE_OFFSET, (SCREEN_HEIGHT - RECT_HEIGHT)/2.0);
 
 
-    let mut position_ball = Vector2 { x: (SCREEN_WIDTH - BALL_RAD)/ 2.0 , y: (SCREEN_HEIGHT - BALL_RAD)/ 2.0 };
+    let mut position_ball = Vector2::new((SCREEN_WIDTH)/ 2.0 , (SCREEN_HEIGHT)/ 2.0);
 
     fn cap_speed(speed: &mut f32, max_speed: f32) {
         if *speed > max_speed {
             *speed = max_speed;
         }
+    }
+
+    fn reset_ball(rl: &mut RaylibHandle, pos: &mut Vector2, speed: &mut f32, dir: &mut Vector2, screen_w: f32, screen_h: f32){
+        *pos = Vector2::new(screen_w / 2.0, screen_h / 2.0);
+        *speed = 500.0;
+
+        let mut angle = 0;
+        while angle % 90 == 0 || (angle > 75 && angle < 105) || (angle > 255 && angle < 285) {angle = rl.get_random_value(1..360);}
+        let radians: f32 = (angle as f32) * DEG2RAD as f32;
+
+        *dir = Vector2::new(radians.cos(), radians.sin());
     }
 
     while !rl.window_should_close() {
@@ -61,17 +72,13 @@ fn main() {
 
 //==================== start of ball logic --------
         if position_ball.x < BALL_RAD{
-        position_ball.x = BALL_RAD;
-        direction_ball.x *= -1.0;
         score_right += 1;
-        cap_speed(&mut speed_ball, MAX_SPEED);
+        reset_ball(&mut rl, &mut position_ball, &mut speed_ball, &mut direction_ball, SCREEN_WIDTH, SCREEN_HEIGHT);
         }
 
         if position_ball.x + BALL_RAD > SCREEN_WIDTH {
-        position_ball.x = SCREEN_WIDTH - BALL_RAD;
-        direction_ball.x *= -1.0;
         score_left += 1;
-        cap_speed(&mut speed_ball, MAX_SPEED);
+        reset_ball(&mut rl, &mut position_ball, &mut speed_ball, &mut direction_ball, SCREEN_WIDTH, SCREEN_HEIGHT);
         }
 
         if position_ball.y < BALL_RAD{
@@ -95,22 +102,24 @@ fn main() {
         d.draw_text(&score_right.to_string(), SCREEN_WIDTH as i32 - 100, 50, 100, Color::WHITE);
         d.draw_text(&score_left.to_string(), 50, 50, 100, Color::WHITE);
 
-        let player_1 = Rectangle { x: position_pdl_right.x, y: position_pdl_right.y, width: RECT_WIDTH, height: RECT_HEIGHT };
-        let player_2 = Rectangle { x: position_pdl_left.x, y: position_pdl_left.y, width: RECT_WIDTH, height: RECT_HEIGHT };
+        let player_1 = Rectangle::new(position_pdl_right.x, position_pdl_right.y, RECT_WIDTH, RECT_HEIGHT);
+        let player_2 = Rectangle::new(position_pdl_left.x, position_pdl_left.y, RECT_WIDTH, RECT_HEIGHT);
         d.draw_rectangle_rec(player_1, Color::RED);
         d.draw_rectangle_rec(player_2, Color::BLUE);
-        let ball_center = Vector2 { x: position_ball.x, y: position_ball.y };
+        let ball_center = Vector2::new(position_ball.x, position_ball.y);
         let ball_radius : f32 = BALL_RAD;
 
         if player_1.check_collision_circle_rec(ball_center, ball_radius){
             direction_ball.x *= -1.0;
             speed_ball *= 1.01;
+            cap_speed(&mut speed_ball, MAX_SPEED);
             position_ball.x = player_1.x - ball_radius;
         }
 
         if player_2.check_collision_circle_rec(ball_center, ball_radius){
             direction_ball.x *= -1.0;
             speed_ball *= 1.01;
+            cap_speed(&mut speed_ball, MAX_SPEED);
             position_ball.x = player_2.x + player_2.width + ball_radius;
         }
         d.draw_fps(10, 10);
