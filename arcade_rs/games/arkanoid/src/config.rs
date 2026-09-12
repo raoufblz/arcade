bece3ce41e90cab0cc3d44e327a434c9ee7c69ec
raoufblz <108367862+raoufblz@users.noreply.github.com>
2026-09-12 21:36:19 +0100

    better speed

--- arcade_rs/games/arkanoid/src/config.rs
+++ arcade_rs/games/arkanoid/src/config.rs
@@ -5,12 +5,12 @@ pub const 	SCREEN_HEIGHT 	:f32 	= 800.0;
 
 pub const 	PADDLE_WIDTH  	:f32 	= 150.0;
 pub const 	PADDLE_HEIGHT 	:f32 	= 30.0;
-pub const 	PADDLE_SPEED  	:f32 	= 500.0;
+pub const 	PADDLE_SPEED  	:f32 	= 800.0;
 
 pub const 	BALL_RADIUS   	:f32 	= 20.0;
 pub const	BALL_SPEED	  	:f32 	= 500.0;
-pub const 	MAX_SPEED	  	:f32 	= 1500.0;
-pub const 	SPEED_INCREMENT	:f32 	= 1.01;
+pub const 	MAX_SPEED	  	:f32 	= 1200.0;
+pub const 	SPEED_INCREMENT	:f32 	= 1.02;
 
 pub const 	BRICK_WIDTH  	:f32 	= 80.0;
 pub const 	BRICK_HEIGHT 	:f32 	= 30.0;
