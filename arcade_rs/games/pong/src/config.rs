452d550458e0b3f8f6a714b340439d7a2e3ab53d
raoufblz <108367862+raoufblz@users.noreply.github.com>
2026-09-13 22:27:48 +0100

    speed fixes

--- arcade_rs/games/pong/src/config.rs
+++ arcade_rs/games/pong/src/config.rs
@@ -5,12 +5,12 @@ pub const SCREEN_HEIGHT		: f32 = 900.0;
 
 pub const PADDLE_WIDTH		: f32 = 35.0;
 pub const PADDLE_HEIGHT		: f32 = 125.0;
-pub const PADDLE_SPEED		: f32 = 400.0;
+pub const PADDLE_SPEED		: f32 = 700.0;
 pub const PADDLE_OFFSET		: f32 = 50.0;
 
 pub const BALL_RAD			: f32 = 30.0;
 pub const BALL_SPEED		: f32 = 500.0;
-pub const SPEED_INCREMENT	: f32 = 1.01;
-pub const MAX_SPEED			: f32 = 1500.0;
+pub const SPEED_INCREMENT	: f32 = 1.02;
+pub const MAX_SPEED			: f32 = 1200.0;
 
 pub const WIN_SCORE			: i32 = 5;
